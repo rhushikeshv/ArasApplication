96eb795d034f48105e88b8013c58f18b577ba7a3
Rhushikesh <Admin@DESKTOP-56EC41H>
2023-11-04 15:41:53 +0530

    application structure added for UI

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -12,16 +12,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'ArasApplication'`, () => {
+  it(`should have as title 'ArasApp'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('ArasApplication');
+    expect(app.title).toEqual('ArasApp');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('ArasApplication app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('ArasApp app is running!');
   });
 });
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,64 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+import { ButtonModule } from 'primeng/button';
+import { LoginComponent } from './login/login.component';
+import {PasswordModule} from "primeng/password";
+import {FormsModule} from "@angular/forms";
+import {InputTextModule} from "primeng/inputtext";
+import {DropdownModule} from "primeng/dropdown";
+import {HTTP_INTERCEPTORS, HttpClientModule} from "@angular/common/http";
+import { HomeComponent } from './home/home.component';
+import {AppRoutingModule} from "./app-routing";
+import {BlockUI, BlockUIModule} from "primeng/blockui";
+import {PanelModule} from "primeng/panel";
+import {SliderModule} from "primeng/slider";
+import {SlideMenuModule} from "primeng/slidemenu";
+import {TabMenuModule} from "primeng/tabmenu";
+import { PartComponent } from './part/part.component';
+import { DrawingComponent } from './drawing/drawing.component';
+import { EcrComponent } from './ecr/ecr.component';
+import {ArasRestApiInterceptorInterceptor} from "./aras-rest-api-interceptor.interceptor";
+import {TableModule} from "primeng/table";
+import {Card, CardModule} from "primeng/card";
+import {LoginService} from "./login/login.service";
+import {RoleBasedDirective} from "./directive/RoleBasedDirective";
+import {StyleClassModule} from "primeng/styleclass";
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    HomeComponent,
+    PartComponent,
+    DrawingComponent,
+    EcrComponent,
+    RoleBasedDirective
+
   ],
-  imports: [
-    BrowserModule
+  providers:[
+    {
+      provide: HTTP_INTERCEPTORS,useClass: ArasRestApiInterceptorInterceptor,multi:true,
+    }
   ],
-  providers: [],
+    imports: [
+        BrowserModule,
+        BrowserAnimationsModule,
+        ButtonModule,
+        PasswordModule,
+        InputTextModule,
+        FormsModule,
+        DropdownModule,
+        HttpClientModule,
+        AppRoutingModule,
+        BlockUIModule,
+        PanelModule,
+        SlideMenuModule,
+        TabMenuModule,
+        TableModule,
+        CardModule,
+        StyleClassModule
+    ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
