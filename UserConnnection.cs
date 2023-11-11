f132517cc86f004f7748c04b9ef7d09a0dd426c2
Rhushikesh <Admin@DESKTOP-56EC41H>
2023-11-11 09:08:39 +0530

    connection to to ARAS OOTB

---
+++ UserConnnection.cs
@@ -0,0 +1,18 @@
+﻿using System.Text.Json.Serialization;
+
+namespace ArasMicroService
+{
+    
+    public class UserConnnection
+    {
+        public string username = String.Empty;
+        public string password = String.Empty;     
+        public string hostname = String.Empty;
+        public string database = String.Empty;
+        public string connection = String.Empty;
+
+       
+       
+
+    }
+}
