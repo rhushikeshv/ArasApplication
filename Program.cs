4134f757b65e102a59c5c1cb2e666f2dcc62177e
Rhushikesh <Admin@DESKTOP-56EC41H>
2023-11-11 09:07:50 +0530

    Start rest services

---
+++ Program.cs
@@ -0,0 +1,58 @@
+
+
+using Microsoft.Extensions.Caching.Distributed;
+using Microsoft.Extensions.Caching.StackExchangeRedis;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllersWithViews();
+builder.Services.AddSession();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: MyAllowSpecificOrigins,
+                      policy =>
+                      {
+                          policy.WithOrigins("http://localhost:4200"
+                                              ).AllowAnyHeader()
+                                                  .AllowAnyMethod(); 
+                      });
+});
+
+
+builder.Services.AddStackExchangeRedisCache(options =>
+{
+    options.Configuration = options.Configuration = "localhost:6379";
+
+});
+
+builder.Services.Add(ServiceDescriptor.Singleton<IDistributedCache, RedisCache>());
+builder.Services.AddHttpClient();
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.UseSession();
+app.UseCors(MyAllowSpecificOrigins);
+
+app.Run();
